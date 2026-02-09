475c9df3064c68bcc0fe3062f041f9a3b2afe1b7
Angel Fabian Melendres Sanaguano <angel.melendres97@gmail.com>
2026-02-09 17:07:59 -0500

    Ajusta totales por linea en orden de compra

--- resources/views/filament/resources/orden-compra-resource/components/resumen-totales.blade.php
+++ resources/views/filament/resources/orden-compra-resource/components/resumen-totales.blade.php
@@ -5,15 +5,15 @@
     $totalGeneral = $totalGeneral ?? 0;
 @endphp
 
-<table class="w-full text-sm">
-    <tbody>
+<table class="w-full text-sm" data-oc-summary>
+    <tbody data-oc-summary-body>
         <tr>
             <th class="text-right font-semibold pr-4">Subtotal</th>
-            <td class="text-right font-bold w-32">$ {{ number_format($subtotalGeneral, 2, '.', ',') }}</td>
+            <td class="text-right font-bold w-32" data-oc-summary-subtotal>$ {{ number_format($subtotalGeneral, 2, '.', ',') }}</td>
         </tr>
         <tr>
             <th class="text-right font-semibold pr-4">Total Descuento</th>
-            <td class="text-right font-bold w-32">$ {{ number_format($descuentoGeneral, 2, '.', ',') }}</td>
+            <td class="text-right font-bold w-32" data-oc-summary-descuento>$ {{ number_format($descuentoGeneral, 2, '.', ',') }}</td>
         </tr>
         @foreach ($tarifas ?? [] as $rate)
             @php
@@ -34,7 +34,7 @@
         @endforeach
         <tr class="border-t border-gray-300 dark:border-gray-700">
             <th class="text-right font-extrabold text-lg text-primary-600 pr-4">Total</th>
-            <td class="text-right font-extrabold text-xl text-primary-600 w-32">$ {{ number_format($totalGeneral, 2, '.', ',') }}</td>
+            <td class="text-right font-extrabold text-xl text-primary-600 w-32" data-oc-summary-total>$ {{ number_format($totalGeneral, 2, '.', ',') }}</td>
         </tr>
     </tbody>
 </table>
